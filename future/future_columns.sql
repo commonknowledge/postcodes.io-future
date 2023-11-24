6ab241ea0ace71d7228e39f795ba0c9da8170b59
ev-sc <4164774+ev-sc@users.noreply.github.com>
2023-11-24 04:56:15 +0100

    Add separate table for 2025

--- future/future_columns.sql
+++ future/future_columns.sql
@@ -1,2 +1,6 @@
 ALTER TABLE postcodes ADD COLUMN constituency_id_2025 varchar(50);
 ALTER TABLE outcodes ADD COLUMN parliamentary_constituency_2025 varchar(200);
+CREATE TABLE constituencies_2025 (
+  code varchar(50) NOT NULL,
+  name varchar(255)
+);
