8c33b7422d052ea878dfc69f670edfa4d71c2a8e
ev-sc <4164774+ev-sc@users.noreply.github.com>
2023-11-24 22:36:18 +0100

    Add crucial indexes

--- future/future_columns.sql
+++ future/future_columns.sql
@@ -1,6 +1,12 @@
 ALTER TABLE postcodes ADD COLUMN constituency_id_2025 varchar(50);
-ALTER TABLE outcodes ADD COLUMN parliamentary_constituency_2025 varchar(200);
+ALTER TABLE outcodes ADD COLUMN parliamentary_constituency_2025 varchar(200)[];
 CREATE TABLE constituencies_2025 (
   code varchar(50) NOT NULL,
   name varchar(255)
 );
+ALTER TABLE ONLY public.constituencies_2025
+    ADD CONSTRAINT constituencies_2025_code_key UNIQUE (code);
+CREATE UNIQUE INDEX constituencies_2025_code_idx ON public.constituencies_2025 USING btree (code);
+CREATE INDEX postcodes_pc_compact ON postcodes(pc_compact);
+CREATE INDEX postcodes_outcode ON postcodes(outcode);
+CREATE INDEX postcodes_postcode ON postcodes(postcode);
